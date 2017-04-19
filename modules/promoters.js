546736fcf1b54a36bb6c0579a58e59e96c1d40be
Cielito <cielito-cosme@users.noreply.github.com>
2017-04-19 15:24:30 +0800

    Update promoters.js

--- modules/promoters.js
+++ modules/promoters.js
@@ -1,133 +1,126 @@
 var request = require('request'),
-	async = require('async'),
-	_ = require('underscore'),
-	Cloudinary = require('./Cloudinary/Cloudinary'),
-	MapResponse = require('../response_body_mapper/MapResponse'),
-	constants = require('../constants');
+ async = require('async'),
+ _ = require('underscore'),
+ Cloudinary = require('./Cloudinary/Cloudinary'),
+ MapResponse = require('../response_body_mapper/MapResponse'),
+ constants = require('../constants');
 
 const URL = constants.BASE_PATH + constants.API_PATH + "/organisations";
 
 function getPromoters(params, callback) {
-	var organisationId = params.organisationId;
-	var getUrl = URL+"/"+organisationId+"/promoters";
-	if(params.promoterId) {
-		getUrl = getUrl + "/"+params.promoterId;
-	}
+ var organisationId = params.organisationId;
+ var getUrl = URL+"/"+organisationId+"/promoters";
+ if(params.promoterId) {
+  getUrl = getUrl + "/"+params.promoterId;
+ }
 
-	var options = {
-		url: getUrl,
-		qs: _.omit(params, ['organisationId'])
-	};
-
-	request.get(options, function (error, response, body) {
-		if(error == null && response.statusCode == constants.SUCCESS) {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
-			callback(null, response, newBody);
-		} else {
-			callback(error, response, null);
-		}
-	});
+ request.get(getUrl, function (error, response, body) {
+  if(error == null && response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+   callback(null, response, newBody);
+  } else {
+   callback(error, response, null);
+  }
+ });
 };
 
 function create(params, callback) {
-	var createUrl = URL +"/" +params.organisationId + "/promoters";
-	params = _.omit(params, 'organisationId');
+ var createUrl = URL +"/" +params.organisationId + "/promoters";
+ params = _.omit(params, 'organisationId');
 
-	var options = {
-		method: 'post',
-		body: params,
-		json: true,
-		url: createUrl
-	};
+ var options = {
+  method: 'post',
+  body: params,
+  json: true,
+  url: createUrl
+ };
 
-	request(options, function (error, response, body) {
-		if(!error && response.statusCode == constants.SUCCESS) {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
+ request(options, function (error, response, body) {
+  if(!error && response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
 
-			callback(null, response, newBody);
-		} else {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
+   callback(null, response, newBody);
+  } else {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
 
-			callback(newBody, response, null);
-		}
-	});
+   callback(newBody, response, null);
+  }
+ });
 };
 
+function updatePromoter(params, callback) {
+ var updateUrl = URL +"/" +params.organisationId + "/promoters/" + params.promoterId;
+ params = _.omit(params, ['organisationId', 'promoterId']);
+
+ var options = {
+  method: 'put',
+  body: params,
+  json: true,
+  url: updateUrl
+ };
+
+ request(options, function (error, response, body) {
+  if(!error && response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+
+   callback(null, response, newBody);
+  } else {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+
+   callback(newBody, response, null);
+  }
+ });
+};
 
 function deletePromoter(params, callback) {
-	var deleteUrl = URL +"/" +params.organisationId + "/promoters/"+ params.promoterId;
-	var options = {
-		method: 'delete',
-		json: true,
-		url: deleteUrl
-	}
+ var deleteUrl = URL +"/" +params.organisationId + "/promoters/"+ params.promoterId;
+ var options = {
+  method: 'delete',
+  json: true,
+  url: deleteUrl
+ }
 
-	request(options, function (error, response, body) {
-		if(!error && response.statusCode == constants.SUCCESS) {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
+ request(options, function (error, response, body) {
+  if(!error && response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
 
-			callback(null, response, newBody);
-		} else {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
+   callback(null, response, newBody);
+  } else {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
 
-			callback(newBody, response, null);
-		}
-	});
+   callback(newBody, response, null);
+  }
+ });
 
 }
 
-
-function updatePromoter(params, callback) {
-	var updateUrl = URL +"/" +params.organisationId + "/promoters/" + params.promoterId;
-	params = _.omit(params, ['organisationId', 'promoterId']);
-
-	var options = {
-		method: 'put',
-		body: params,
-		json: true,
-		url: updateUrl
-	};
-
-	request(options, function (error, response, body) {
-		if(!error && response.statusCode == constants.SUCCESS) {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
-
-			callback(null, response, newBody);
-		} else {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
-
-			callback(newBody, response, null);
-		}
-	});
-};
-
 function getPromoterByCode(params, callback) {
-	var organisationId = params.organisationId;
-	var getUrl = URL+"/"+organisationId+"/promoter-code/"+params.promoterCode;
+ var organisationId = params.organisationId;
+ var getUrl = URL+"/"+organisationId+"/promoter-code/"+params.promoterCode;
 
-	request.get(getUrl, function (error, response, body) {
-		
-		if(response.statusCode == constants.SUCCESS) {
-			var mapResponse = new MapResponse(body);
-			var newBody = mapResponse.mapData();
-			callback(null, response, newBody);
-		} else {
-			callback(body, response, null);
-		}
-	});
+ request.get(getUrl, function (error, response, body) {
+  
+  if(response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+   callback(null, response, newBody);
+  } else {
+   callback(body, response, null);
+  }
+ });
 }
 
 module.exports = {
-	getPromoters: getPromoters,
-	create: create,
-	updatePromoter: updatePromoter,
-	deletePromoter: deletePromoter,
-	getPromoterByCode: getPromoterByCode
+ getPromoters: getPromoters,
+ create: create,
+ updatePromoter: updatePromoter,
+ deletePromoter: deletePromoter, 
+ getPromoterByCode: getPromoterByCode
 }
