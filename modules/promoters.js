26b77633215cb19d835bf0edc067a9412dd7d687
Cielito <cielito-cosme@users.noreply.github.com>
2017-04-20 16:28:03 +0800

    Update promoters.js

--- modules/promoters.js
+++ modules/promoters.js
@@ -77,6 +77,30 @@ function updatePromoter(params, callback) {
  });
 };
 
+function deletePromoter(params, callback) {
+ var deleteUrl = URL +"/" +params.organisationId + "/promoters/"+ params.promoterId;
+ var options = {
+  method: 'delete',
+  json: true,
+  url: deleteUrl
+ }
+
+ request(options, function (error, response, body) {
+  if(!error && response.statusCode == constants.SUCCESS) {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+
+   callback(null, response, newBody);
+  } else {
+   var mapResponse = new MapResponse(body);
+   var newBody = mapResponse.mapData();
+
+   callback(newBody, response, null);
+  }
+ });
+
+}
+
 function getPromoterByCode(params, callback) {
  var organisationId = params.organisationId;
  var getUrl = URL+"/"+organisationId+"/promoter-code/"+params.promoterCode;
@@ -97,5 +121,6 @@ module.exports = {
  getPromoters: getPromoters,
  create: create,
  updatePromoter: updatePromoter,
+ deletePromoter: deletePromoter, 
  getPromoterByCode: getPromoterByCode
 }
